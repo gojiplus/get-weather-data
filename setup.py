419ddabe20945d92fe8537566e305c98b26b4590
Suriyan <suriyant@gmail.com>
2016-07-17 07:34:30 +0700

    use rtd theme locally

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.29',
+    version='0.1.30',
 
     description='Scripts for finding out the weather in a particular zip code',
     long_description=long_description,
